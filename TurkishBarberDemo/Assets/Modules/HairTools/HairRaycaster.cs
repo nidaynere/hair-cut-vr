d4190b9e9b9dfedce9a10dcbbae85c334d2f5fa1
unknown <nidaynere@gmail.com>
2023-03-19 00:12:12 -0700

    brush size back

--- TurkishBarberDemo/Assets/Modules/HairTools/HairRaycaster.cs
+++ TurkishBarberDemo/Assets/Modules/HairTools/HairRaycaster.cs
@@ -12,7 +12,7 @@ namespace HairTools {
             var layer = hairBaker.SurfaceLayer;
 
             if (Physics.SphereCast (
-                ray, 1, out var hit, Mathf.Infinity, 1 << layer)) {
+                ray, brushSize, out var hit, Mathf.Infinity, 1 << layer)) {
                 point = hit.point;
                 return true;
             }
